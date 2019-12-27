fa6206e2ad6b08974c16544bfa431fa40480cbca
Robert Keller <robert_keller44@icloud.com>
2019-12-27 16:57:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,8 +86,6 @@ def adding_matz
                         :known_for => "Ruby",
                         :languages => ["LISP", "C"]
   }]
-                      
-  puts :yukihiro_matsumoto
 end
 
 def changing_alan
